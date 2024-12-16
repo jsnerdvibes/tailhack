ad3d07e9ca3e76622ec52ac93f02ec248aa6b004
JS Nerd <shaikhdanish1710@gmail.com>
2024-12-16 16:17:56 +0530

    created path for script and style to serve files as static files are not working properly on vercel

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import express from 'express'
 import 'dotenv/config'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 
@@ -12,6 +19,13 @@ app.get('/',(req,res)=>{
     res.send('server is up')
 })
 
+app.get('/script',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public','script.js'))
+})
+
+app.get('/style',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public','style.css'))
+})
 
 app.listen(port,()=>{
     console.log("server is up at",port)
